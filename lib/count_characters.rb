8ee57eb5347f15b3fd1cd9ec8542b40c8b40dafd
Deborah Hamel <deborahleehamel.gmail>
2015-12-21 20:53:25 -0700

    Building up to what needed for unit testing.

--- lib/count_characters.rb
+++ lib/count_characters.rb
@@ -1,3 +1,25 @@
-puts "a: 3"
-puts "b: 2"
-puts "c: 1"
+class CountCharacters
+  def initialize(string)
+  end
+
+  def formatted_counts
+    "a: 3\n" +
+    "b: 2\n" +
+    "c: 1\n"
+  end
+end
+
+# count the characters in the argument
+# how do I want to count the characters? - maybe a class for that
+# make a thing that counts characters 'CountCharacters.new' -
+# give it a string/input(argument)
+# and ask it for the count .count
+count_characters = CountCharacters.new(ARGV[0])
+
+# format them for printing - want to push this done not at top
+# asking class 'count_characters' for the 'formatted_count'
+counts = count_characters.formatted_counts
+
+
+# print them
+puts counts
